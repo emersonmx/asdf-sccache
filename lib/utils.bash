4eaf5b8f1c6f6329e28b247d14cd8ebe8645d4b6
Emerson MX <emersonmx@gmail.com>
2021-01-31 17:23:43 -0300

    Add quotes

--- lib/utils.bash
+++ lib/utils.bash
@@ -76,7 +76,7 @@ install_version() {
     local tool_cmd
     tool_cmd="$(echo "sccache --help" | cut -d' ' -f1)"
     tool_path="$install_path/$tool_cmd"
-    chmod +x $tool_path
+    chmod +x "$tool_path"
 
     echo "sccache $version installation was successful!"
   ) || (
